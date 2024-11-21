921ae4611a4692297a645e81f9056c416316b36b
Miroslav Mitrovic <stainnn@gmail.com>
2024-11-22 00:51:38 +0100

    Initial commit

---
+++ apps/codegenerato/src/app/placeholder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-placeholder',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './placeholder.component.html',
+  styleUrl: './placeholder.component.scss',
+})
+export class PlaceholderComponent {}
